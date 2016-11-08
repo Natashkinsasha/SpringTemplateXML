b79d01380731d51d17450732e7e956379806b2de
Natashkinsasha <natashkinsasha@gmail.com>
2016-11-08 04:02:01 +0300

    Innit commi

--- src/main/java/by/natashkinsasha/springtemplatexml/entity/WaybillStatus.java
+++
@@ -1,7 +0,0 @@
-package by.natashkinsasha.springtemplatexml.entity;
-
-
-public enum WaybillStatus {
-
-    JOINED, CHECKING_COMPLETED, REGISTRATION_COMPLETED
-}
--- src/main/java/by/natashkinsasha/springtemplatexml/repository/WaybillRepository.java
+++
@@ -1,9 +0,0 @@
-package by.natashkinsasha.springtemplatexml.repository;
-
-
-
-import by.natashkinsasha.springtemplatexml.entity.Waybill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WaybillRepository extends JpaRepository<Waybill, Long>{
-}
